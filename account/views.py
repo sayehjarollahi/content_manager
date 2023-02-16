1049704a6d0390bee791fbd00c3928036a6e409f
sayehjarollahi <sjarollahi@gmail.com>
2023-02-16 11:22:51 +0330

    feat: Add login logic

--- account/views.py
+++ account/views.py
@@ -1,11 +1,21 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from account.forms import SignUpForm
+from account.forms import RegisterForm
 
 
 # Sign Up View
-class SignUpView(CreateView):
-    form_class = SignUpForm
-    success_url = reverse_lazy('signup')
-    template_name = 'signup.html'
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy('register')
+    template_name = 'register.html'
+
+
+def index(request):
+    context = {
+        'is_authenticated': request.user.is_authenticated,
+        'username': request.user.username,
+    }
+
+    return render(request, 'index.html', context=context)
--- content_manager/urls.py
+++ content_manager/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from account.views import SignUpView
+from account.views import RegisterView, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('', index, name='index'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', include("django.contrib.auth.urls")),
 ]
